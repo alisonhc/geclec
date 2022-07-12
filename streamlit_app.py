3a7d5e12b1483755b1ead87bc377e175c3b26066
alisonhc <alisonhchi@gmail.com>
2022-07-12 15:09:47 +0800

    url updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,10 +12,16 @@ geclec_t5_model = AutoModelForSeq2SeqLM.from_pretrained(geclec_t5_path)
 
 st.title('GEC + LEC')
 
+query_params = st.experimental_get_query_params()
+value = ""
+if query_params and not value:
+    value = query_params['input'][0]
+
 sent = st.text_area('Input Sentence', placeholder="The main objectives of this colleges are to help people learn knowledge, "
-                                     "to strengthen their cultural competences, "
-                                     "and to participate in the growth of nearby industry.")
+                                                  "to strengthen their cultural competences, "
+                                                  "and to participate in the growth of nearby industry.", value=value)
 
 if st.button('Correct Sentence'):
     res = inference.correct_sent(model=geclec_t5_model, tok=geclec_t5_tok, sent=sent)
+    st.experimental_set_query_params(input=sent)
     st.write(res)
